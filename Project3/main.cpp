deab8e8b68a0932b5ef48ad2e5cdd869895fc1bf
ugurcanpolat <polatu@itu.edu.tr>
2017-11-24 00:59:25 +0300

    Read lookup file and do operations

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -21,18 +21,19 @@
 using namespace std;
 
 int main() {
-    ifstream input("ds-set-input.txt", ifstream::in); // Read
+    ifstream inputFile("ds-set-input.txt", ifstream::in); // Read
     
-    if (!input.is_open()) {
+    if (!inputFile.is_open()) {
         cout << endl << "Error opening input file." << endl << endl;
         return 0;
     }
     
     string line;
     Dictionary dictionary;
+    List list;
     
-    while(!input.eof()) {
-        getline(input, line); // Read the line
+    while(!inputFile.eof()) {
+        getline(inputFile, line); // Read the line
         
         // If line is empty, no need to take action
         if(line.empty()) break;
@@ -48,11 +49,51 @@ int main() {
         getline(linestream, read[2], '\t');
         linestream >> read[3];
         
-        // Create a new BookCharacter and insert it to
+        // Create a new BookCharacter
         BookCharacter new_book_character(read);
         dictionary.insert(new_book_character);
+        list.insert(new_book_character);
+    }
+    inputFile.close();
+    
+    ifstream lookupFile("ds-set-lookup.txt", ifstream::in); // Read
+    
+    if (!lookupFile.is_open()) {
+        cout << endl << "Error opening lookup file." << endl << endl;
+        return 0;
     }
     
-    input.close();
+    Dictionary lookupDictionary;
+    List lookupList;
+    
+    while(!lookupFile.eof()) {
+        getline(lookupFile, line); // Read the line
+        
+        // If line is empty, no need to take action
+        if(line.empty()) break;
+        
+        // Create stringstream to parse with ','
+        stringstream linestream(line);
+        
+        string read[4];
+        
+        // Parse the line
+        getline(linestream, read[0], '\t');
+        getline(linestream, read[1], '\t');
+        linestream >> read[2];
+        
+        // Create a new BookCharacter
+        BookCharacter new_book_character(read);
+        
+        char chr = dictionary.lookup(new_book_character);
+        read[3] = chr;
+        lookupDictionary.insert(new_book_character);
+        
+        chr = list.lookup(new_book_character);
+        read[3] = chr;
+        lookupList.insert(new_book_character);
+    }
+    lookupFile.close();
+    
     return 0;
 }
