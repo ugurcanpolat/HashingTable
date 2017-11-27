0be5e186a92a17b272c8d7a7ec11186c31b0e525
ugurcanpolat <polatu@itu.edu.tr>
2017-11-27 14:18:53 +0300

    Implement a function that writes output data to file

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -13,13 +13,15 @@
 #include <fstream> // ifstream
 #include <sstream> // stringstream
 #include <string> // string
-#include <vector>
+#include <vector> // vector
 
 #include "p3char.cpp"
 #include "p3dict.cpp"
 #include "p3list.cpp"
 
 using namespace std;
+
+void writeToOutputFile(string fileName, vector<BookCharacter> data);
 
 int main() {
     ifstream inputFile("ds-set-input.txt", ifstream::in); // Read
@@ -89,26 +91,34 @@ int main() {
         char chr = dictionary.lookup(new_book_character);
         new_book_character.setCharacter(chr);
         lookupDictionary.push_back(new_book_character);
+        
+        chr = list.lookup(new_book_character);
+        lookupList.push_back(new_book_character);
     }
     lookupFile.close();
     
-    ofstream output("ds-set-output-dict.txt", ofstream::out); // Write
+    writeToOutputFile("ds-set-output-dict.txt", lookupDictionary);
+    writeToOutputFile("ds-set-output-list.txt", lookupList);
+    
+    return 0;
+}
+
+void writeToOutputFile(string fileName, vector<BookCharacter> data) {
+    ofstream output(fileName, ofstream::out); // Write
     
     if (!output.is_open()) {
-        cout << endl << "Error opening output file." << endl << endl;
-        return 0;
+        cout << endl << "Error opening output file: " << fileName << endl << endl;
+        return;
     }
     
     // Write the sorted segment values
-    for (int counter = 0; counter < lookupDictionary.size(); counter++) {
-        output << lookupDictionary[counter].getPageNo() << '\t';
-        output << lookupDictionary[counter].getLineNo() << '\t';
-        output << lookupDictionary[counter].getIndex()<< '\t';
-        output << lookupDictionary[counter].getCharacter() << endl;
+    for (int counter = 0; counter < data.size(); counter++) {
+        output << data[counter].getPageNo() << '\t';
+        output << data[counter].getLineNo() << '\t';
+        output << data[counter].getIndex()<< '\t';
+        output << data[counter].getCharacter() << endl;
     }
     
     // Close the output file since it is no longer needed
     output.close();
-    
-    return 0;
 }
